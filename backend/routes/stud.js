c5ba4219d24b22c7bfd1e0989d3f81960cc95cc2
rahulchavan62 <rahulchavan7072rc@gmail.com>
2025-07-21 23:24:08 +0530

    Fix login error key and typo

--- backend/routes/stud.js
+++ backend/routes/stud.js
@@ -72,26 +72,25 @@ router.put("/students/:id", async (req, res) => {
   }
 });
 
-router.post("/login",async(req,res)=> {
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-    const {username,password}=req.body;
-    
-    try {
-        const admin=await Admin.findOne({username})
+  try {
+    const admin = await Admin.findOne({ username });
 
+    if (!admin)
+      return res.status(404).json({ message: "Admin not found" });
 
-        if(!admin) return res.status(404).json({message:"Admin not found"})
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
 
-            const isMatch=await bcrypt.compare(password,admin.password)
-            if(!isMatch) return res.status(400).json({Message:"Invalid creadentials"})
-
-                const token=jwt.sign({id:admin._id},"SECRET_KEY",{expiresIn:"1h"})
-                res.json({token});
-    }
-    catch(error) {
-        res.status(500).json({message:error.message});
-    }
-})
+    const token = jwt.sign({ id: admin._id }, "SECRET_KEY", { expiresIn: "1h" });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
 
